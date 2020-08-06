765546c13ddb16adad3e563226dc4368920c17c1
raianwz <raian03@live.com>
2020-08-05 22:38:19 -0300

    Adicionado Formulario

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,7 +9,7 @@ function Footer() {
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura"></img>
       </a>
       <p>
-        Orgulhosamente criado durante a
+        Orgulhosamente criado por RaianWZ durante a
         {' '}
         <a href="https://www.alura.com.br/">
           Imersão React da Alura
