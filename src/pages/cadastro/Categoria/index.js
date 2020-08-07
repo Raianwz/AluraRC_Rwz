29b2e10b8ef24cb436f9d11ed6b9890d341880d2
raianwz <raian03@live.com>
2020-08-07 14:47:36 -0300

    Pequenos Ajustes

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -28,8 +28,9 @@ function CadastroCategoria() {
     );
   }
   useEffect(() => {
-    console.log('teste');
-    const URL_TOP = 'http://localhost:8080/categorias';
+    const URL_TOP = window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categorias'
+      : 'https://rwzflix.herokuapp.com/categorias';
     fetch(URL_TOP)
       .then(async (respostaDoServidor) => {
         const resposta = await respostaDoServidor.json();
