76522b49f9dcf1d586156c3010099464f245c362
raianwz <raian03@live.com>
2020-08-09 21:41:12 -0300

    Ajuste link footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,7 +8,7 @@ function Footer() {
       <a href="https://www.alura.com.br/">
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
       </a>
-      <a href="https:/github.com/raianwz/">
+      <a href="https://github.com/raianwz" rel="noreferrer" target="_blank">
         <ImgLogo src={Logo} alt="Logo RaianWZ" />
       </a>
       <p>
